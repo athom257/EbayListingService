8c3151067e7a2df9cbfe667ae9d21580903285af
athom257 <antone_thomas2000@yahoo.com>
2020-04-26 13:45:09 -0400

    Code refactoring.

--- src/main/java/com/listing/common/ListingManager.java
+++ src/main/java/com/listing/common/ListingManager.java
@@ -40,8 +40,6 @@ public class ListingManager {
                 continue;
             }
 
-            Thread.sleep(2000);
-
             VendorSkuItemDTO vendorSkuItemDTO =
                     vendorSkuItemDAO.queryVendorSkuItem(vendorSku);
 
